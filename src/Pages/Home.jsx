6eeed1b87f828dd41902a6eaad85633a843e6809
gastond90 <94383358+gastond90@users.noreply.github.com>
2023-09-25 19:54:24 -0300

    luns

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,14 +5,20 @@ import fondoportada from '../Img/foto.png'
 import CASCOOBRA from '../Img/CASCO OBRA.png'
 import TIULOOBRA from '../Img/TITULO OBRA.png'
 import fortisLogo from '../Img/fortispalabra.svg'
+import fortisLogoBlanco from '../Img/LogoFortisblanco.png'
 import banda from '../Img/banda_1.gif'
 import flecha from '../Img/flecha.png'
+import flechaBlanca from '../Img/flecha blanca.png'
 import Salta from '../Components/Salta';
 import Cuadrado from '../Components/Cuadrado';
 import casco from '../Img/02 ICONO CASCO.png'
 import flechita from '../Img/04 FLECHA.png'
 import grua from '../Img/03 ICONO GRUA.png'
 import F from '../Img/01 ICONO F.png'
+import fondocontacto from '../Img/fondoContacto.png'
+import evo from '../Img/construccionevolucion.png'
+import Facebook from '../Components/Facebook';
+import Insta from '../Components/Insta';
 
 export default function Home() {
 
@@ -192,21 +198,21 @@ return (
                 </div>
                 
                 <center>
-                    <div className="grid-container lg:pl-32">
-                        <div className="grid-item"><Cuadrado text={"Análisis Y Armado De Proyectos"}/></div>
-                        <div className="grid-item"><Cuadrado text={"Obras De Arquitectura"}/></div>
-                        <div className="grid-item"><Cuadrado text={"Obras De Infraestructura Y Saneamiento"}/></div>
-                        <div className="grid-item"><Cuadrado text={"Obras Hidráulicas"}/></div>
-                        <div className="grid-item"><Cuadrado text={"Obras De Ingeniería"}/></div>
-                        <div className="grid-item"><Cuadrado img={casco}/></div>
-                        <div className="grid-item"><Cuadrado img={flechita}/></div>
-                        <div className="grid-item"><Cuadrado img={F}/></div>
-                        <div className="grid-item"><Cuadrado img={grua}/></div>
-                        <div className="grid-item"><Cuadrado img={F}/></div>
+                    <div className="grid-container lg:pl-52">
+                      <div className="grid-item"><Cuadrado text={"Análisis Y Armado De Proyectos"}/></div>
+                      <div className="grid-item"><Cuadrado text={"Obras De Arquitectura"}/></div>
+                      <div className="grid-item"><Cuadrado text={"Obras De Infraestructura Y Saneamiento"}/></div>
+                      <div className="grid-item"><Cuadrado text={"Obras Hidráulicas"}/></div>
+                      <div className="grid-item"><Cuadrado text={"Obras De Ingeniería"}/></div>
+                      <div className="grid-item"><Cuadrado img={casco}/></div>
+                      <div className="grid-item"><Cuadrado img={flechita}/></div>
+                      <div className="grid-item"><Cuadrado img={F}/></div>
+                      <div className="grid-item"><Cuadrado img={grua}/></div>
+                      <div className="grid-item"><Cuadrado img={F}/></div>
                     </div>
                 </center>
             </div>
-            <div id="Obras" className='bg-azul-100' style={{width:"auto",maxHeight:"100vh",height:"100vh"}}>
+            <div id="Obras" className='bg-azul-100' style={{width:"auto",maxHeight:"100vh"/* ,height:"100vh" */}}>
                 <div className='flex w-full justify-between pl-16 pt-16'>
                     <div className="w-1/2 mx-auto flex flex-col items-center">
                         <img src={TIULOOBRA} alt="" className="w-40"/>
@@ -226,6 +232,65 @@ return (
                     </div>
                 </div>
                 <img src={banda} alt="" className="pb-4 w-full"/>
+            </div>
+            <div id="Contacto" className='pl-8 sm:pl-44' style={{width:"auto",height:"100vh", backgroundImage: `url(${fondocontacto})`, backgroundSize:"cover", /* backgroundRepeat:"repeat-x" */}}>
+                
+                <div className='flex justify-start pt-32 mb-9'>
+                    <img src={fortisLogoBlanco} alt="" className="w-32"/>
+                </div>
+                
+                <div className='flex w-full justify-between'>
+                <div className='w-1/2 overflow-x-auto'>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.356901544587!2d-65.41005502562398!3d-24.783229607614064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x941bc3be7de30015%3A0xaba4e219b40f40d6!2sDe%C3%A1n%20Funes%20570%2C%20A4400EDL%20Salta!5e0!3m2!1ses-419!2sar!4v1694992061911!5m2!1ses-419!2sar" width="450" height="350" /* style="border:0;" */ allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" ></iframe>
+                </div>
+
+                <div className='w-1/2 flex flex-col items-start justify-start'>
+
+                <p className='pcontacto' style={{fontSize:"1.5rem", fontWeight:"bolder"}}>
+                  CONTACTO
+                </p>
+                <p className='pcontacto' style={{fontSize:"0.9rem", fontWeight:"bolder"}}>
+                  +3875 32-6762
+                </p>
+                <p className='pcontacto ' style={{fontSize:"0.9rem"}}>
+                  INSTAGRAM <span style={{ fontWeight:"bolder"}}> FORTIS.SRL</span> 
+                </p>
+
+                <img src={flechaBlanca} alt="" className="w-24 py-8"/>
+                <img src={evo} alt="" className="w-44 pl-8"/>
+
+                </div>
+
+                
+
+                </div>
+
+               
+
+                
+                
+            </div>
+
+            <div id="footer" className='bg-rojo-100' style={{width:"auto",height:"25vh"}}>
+               <div className='w-full h-full flex items-center pl-60'>
+
+                  <div className='flex w-1/3 justify-start'>
+                  <p className='pcontacto pr-4' style={{fontSize:"0.9rem", fontWeight:"bolder"}}>
+                    seguinos  
+                  </p>
+                  <Facebook/>
+                  <Insta/>
+                  </div>
+                  <div className='flex w-1/3'>
+                  <img src={fortisLogoBlanco} alt="" className="w-44"/>
+                  </div>
+                  <div className='flex w-1/3 justify-center'>
+                  <p className='pcontacto' style={{fontSize:"0.9rem", fontWeight:"bolder"}}>
+                    → HABLEMOS!
+                  </p>
+                  </div>
+
+               </div>
             </div>
         </NoAuthLayout>
     </>
