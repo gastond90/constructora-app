20b503e715c8e9996381a91706353069b5c8bafa
Gastón <gduba90@gmail.com>
2023-12-13 11:35:28 -0300

    mmm

--- src/Components/Obras/SingleObra.jsx
+++ src/Components/Obras/SingleObra.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ButtonObra from './ButtonObra';
 import { Link } from 'react-router-dom';
-import { useScreenSize } from '../../Utils/media-query';
 
 function SingleObra({bg, text, link}) {
-
-  const {Lg, XS, isMedium} = useScreenSize() 
-
     return (
-    <div /* className='flex flex-col items-end justify-end  m-2 sm:m-4 m-8 h-48 w-40 sm:w-64 lg:w-80' */ className="flex flex-col items-end justify-end  m-2 sm:m-4 sm:m-8 h-48 w-32 sm:w-full m-4" style={{backgroundImage: `url(${bg})`, backgroundSize:"cover"}}>
+    <div className="flex flex-col items-end justify-end sm:m-4 sm:m-8 h-48 w-44 sm:w-full " style={{backgroundImage: `url(${bg})`, backgroundSize:"cover"}}>
       <Link to={link}>
         <ButtonObra text={text}/>
       </Link>
@@ -16,5 +12,3 @@ function SingleObra({bg, text, link}) {
 }
 
 export default SingleObra;
-
-/* w-40 sm:w-64 lg:w-80 */
